4b4ac74c0ddc29fc08b23992985bdbd1397952e2
Felix Campos <felixcampos63040@gmail.com>
2025-04-27 20:28:19 -0300

    Adicionas dependencia do backend

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,8 +1,10 @@
 // hellohelp-backend/firebase/firebaseAdmin.js
 import admin from "firebase-admin";
 
+// Pega o conteúdo do service account da variável de ambiente
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// Inicializa o app apenas se ainda não foi inicializado
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
